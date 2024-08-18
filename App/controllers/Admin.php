0b8936321c73fb9c72e31bb5e4c288a72127403e
Himantha Marasinghe <himanthamarasinghe@gmail.com>
2024-08-18 20:32:16 +0530

    Improve SideBar
    Update Admin and Customer controllers with SideBar tabs.
    Add new icons to icons.svg.

--- App/controllers/Admin.php
+++ App/controllers/Admin.php
@@ -3,9 +3,14 @@
 class Admin extends Controller 
 {
     protected $data = [
-        'tabs' => ['tabs' => ['Home', 'Loyalty Customers', 'Stocks', 'Accounts'], 'userType' => 'Admin']
+        'tabs' => ['tabs' => ['Home', 'Users', 'Products', 'Announcements'], 'userType' => 'Admin']
     ];
 
     //create new methods after this line.
+
+    public function index()
+    {
+        $this->view('admin/home', $this->data);
+    }
 
 }
--- App/controllers/Customer.php
+++ App/controllers/Customer.php
@@ -8,4 +8,10 @@ class Customer extends Controller
 
     //create new methods after this line.
 
+    public function index()
+    {
+        $this->data['tabs']['active'] = 'Home';
+        $this->view('customer/home', $this->data);
+    }
+
 }
