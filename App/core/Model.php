fb45d54e3c7cb0c26186ada9b7ed117cb51d7036
chenuka13 <140165038+chenuka13@users.noreply.github.com>
2024-08-26 20:21:33 +0530

    Create insert and readall methods

--- App/core/Model.php
+++ App/core/Model.php
@@ -52,4 +52,31 @@ class Model extends Database
 
         return false;
     }
+    public function readAll()
+    {
+        $query = "SELECT * FROM $this->table";
+        return $this->query($query);
+    }
+
+    public function insert($data)
+    {
+
+        // Only the fields that are mentioned in the fillable array will be inserted.
+        if(!empty($this->fillable))
+        {
+            foreach ($data as $key => $value)
+            {
+                if(!in_array($key, $this->fillable))
+                {
+                    unset($data[$key]);
+                }
+            }
+        }
+
+        $keys = array_keys($data);
+        $query = "INSERT INTO $this->table (".implode(",", $keys).") VALUE (:".implode(",:", $keys).")";
+        $this->query($query, $data);
+        return false;
+    }
+    
 }
