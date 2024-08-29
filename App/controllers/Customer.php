dc1e9113253ab1a6de5021a2db0621d822833b89
Himantha Marasinghe <himanthamarasinghe@gmail.com>
2024-08-29 23:32:32 +0530

    Add icons

--- App/controllers/Customer.php
+++ App/controllers/Customer.php
@@ -29,10 +29,4 @@ class Customer extends Controller
     }
     //create new methods after this line.
 
-    public function index()
-    {
-        $this->data['tabs']['active'] = 'Home';
-        $this->view('customer/home', $this->data);
-    }
-
 }
