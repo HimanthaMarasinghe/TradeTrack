4c17bffd4511c2b61652388ce7262d13b10bf06f
M.A.H.K. Marasinghe <himanthamarasinghe@gmail.com>
2024-11-16 00:33:14 +0530

    Created methods to read all the  New loyalty requests

--- App/components/card/productVert.component.php
+++ App/components/card/productVert.component.php
@@ -1,5 +1,9 @@
 <a href="" class="card btn-card colomn asp-rtio">
-        <img class="product-img" src="<?=ROOT?>/images/Products/<?=$barcode?>.<?=$pic_format?>" alt="">
+        <?php if(file_exists("./images/Products/".$barcode.".".$pic_format)){ ?>
+            <img class="product-img" src="<?=ROOT?>/images/Products/<?=$barcode?>.<?=$pic_format?>" alt="">
+        <?php } else { ?>
+            <img class="product-img" src="<?=ROOT?>/images/Products/default.jpg" alt="">
+        <?php } ?>
     <div class="details h-50">
         <h4><?=$product_name?></h4>
         <h4>Rs.<?= number_format($unit_price, 2) ?></h4>
