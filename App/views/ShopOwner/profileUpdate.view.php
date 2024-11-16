46ae8c8048371f14fc8fc4eba33107a94295434a
niro1231 <pratheepanniroshan49 @gmail.com>
2024-11-16 08:39:53 +0530

    new file add

--- App/views/ShopOwner/profileUpdate.view.php
+++ App/views/ShopOwner/profileUpdate.view.php
@@ -6,15 +6,44 @@
 
 <div class="main-content">
 
+<h2>Shop Owner Profile </h2>
 <!-- Your html code goes here -->
  <form action="http://localhost/TradeTrack/FormTest" method="POST">
-    <input class="userInput" type="text" name="a" id="">
-    <input type="text" name="b" id="">
-    <input type="text" name="c" id="">
-    <input type="text" name="d" id="">
-    <input type="text" name="e" id="">
-    <input type="text" name="f" id="">
-    <input type="submit" name="g" id="">
+    
+
+    <table class="Profile">
+            <tr>
+                <td>Shop owner's Phone Number</td>
+                <td><input class="userInput" type="text" name="shopownerPhoneNo" id=""></td>
+            </tr>
+            <tr>
+                <td>Shop Name</td>
+                <td><input class="userInput" type="text" name="shopName" id=""></td>
+            </tr>
+            <tr>
+                <td>Shop owner's  First Name</td>
+                <td><input class="userInput" type="text" name="shopownerFirstName" id=""></td>
+            </tr>
+            <tr>
+                <td>Shop owner's  last name</td>
+                <td><input class="userInput" type="text" name="shopownerLastName" id=""></td>
+            </tr>
+            <tr>
+                <td>shop address </td>
+                <td><input class="userInput" type="text" name="shopaddress" id=""></td>
+            </tr>
+            <tr>
+                <td>shop owner address </td>
+                <td><input class="userInput" type="text" name="Shopowneraddress" id=""></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align:right;">
+                    <input type="submit" class="btn" value="Submit">
+                </td>
+            </tr>
+        </table>
+
+
  </form>
 
 </div>
