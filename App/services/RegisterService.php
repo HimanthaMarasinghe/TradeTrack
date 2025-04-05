aba9efa35ca81d245de856112d86c3a946be09c9
M.A.H.K. Marasinghe <himanthamarasinghe@gmail.com>
2025-04-05 15:53:30 +0530

    ImageUpload method created

--- App/services/RegisterService.php
+++ App/services/RegisterService.php
@@ -2,19 +2,24 @@
 
 class RegisterService extends Database
 {
-    function register($newUser){
+    public function register($newUser){
         $user = new User;
         $userPassword = new UserPasswords;
+        $imgUpl = new ImageUploader;
 
         $con = $this->startTransaction();
         //Transactions should be done using the same connection. Therefore, we pass the connection to the functions that need it.
         $userPassword->insert($newUser, $con);
+
+        $newUser['pic_format'] = $imgUpl->upload('user_image', $newUser['phone'], 'Profile');
         $user->insert($newUser, $con);
 
         switch ($newUser['role']) {            
             case '1':
                 $shop = new Shops;
                 $newUser['so_phone'] = $newUser['phone'];
+                $pic_format = $imgUpl->upload('shop_image', $newUser['phone'], 'Shops');
+                if ($pic_format) $newUser['shop_pic_format'] = ".$pic_format";
                 $shop->insert($newUser, $con);
                 break;
 
